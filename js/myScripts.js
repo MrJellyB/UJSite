36664f3fd76a4adc6fe98a35220c51db9d47fe5d
MrJellyB <shvachernoam@gmail.com>
2014-08-18 17:33:06 +0300

    Adding the comments feature

--- js/myScripts.js
+++ js/myScripts.js
@@ -15,5 +15,42 @@ $(document).ready(function(){
 		queue : false});
 	});
 
-	$(".tabs").tabs({active : 1, collapsible : true});
+	//$(".tabs").tabs({active : 1, collapsible : true});
+
+	// Fix article tabs width as parent height
+	// And position them correctly
+
+	$(".artinav-content > div").height("150px");
+
+	$(".article-navbar").each(function (){
+		var originalParent = $(this).parent();
+
+		$(this).css({
+			"position": "absolute",
+			"z-index": "-1",
+			"left": "0px",
+			"top": "0px"
+		});
+
+		if (originalParent.height() < articleInitHeight) {
+			$(originalParent).height(articleInitHeight);
+			$(this).width(articleInitHeight);
+		};
+
+		$(this).css({
+			width: originalParent.height(),
+			paddingLeft: originalParent.css("padding-top"),
+			paddingRight: originalParent.css("padding-bottom")
+		});
+
+		$(this).position({
+			my: "left top",
+			at: "left top",
+			of: originalParent,
+			collision: "none"
+		});
+
+		$(this).css("-webkit-transform-origin" ,"0% 0%");
+		$(this).css("-webkit-transform", "rotate(90deg)");
+	});
 });
